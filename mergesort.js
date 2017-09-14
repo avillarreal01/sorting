a26cd83040f7364aa58c1fbb76dc6275499b713c
Alejandro Villarreal <avillarreal01@gmail.com>
2017-09-14 13:25:04 -0400

    added mergeSort solution

--- mergesort.js
+++ mergesort.js
@@ -1,36 +1,28 @@
 function split(wholeArray) {
-
-    var firstHalf = wholeArray.slice(0, wholeArray.length/2);
-    var secondHalf = wholeArray.slice(wholeArray.length/2);
+    var firstHalf = wholeArray.slice(0, wholeArray.length / 2);
+    var secondHalf = wholeArray.slice(wholeArray.length / 2);
     return [firstHalf, secondHalf];
   }
 
   function merge(arr1, arr2){
     var output = [];
-    while(arr1.length && arr2.length){
-      if(arr2[0] < arr1[0]){
+    while (arr1.length && arr2.length) {
+      if (arr2[0] < arr1[0]) {
         output.push(arr2.shift());
       } else {
         output.push(arr1.shift());
       }
     }
-    if(arr1.length){
-      output = output.concat(arr1);
-    } else if (arr2.length){
-      output = output.concat(arr2);
-    }
+    if (arr1.length) output = output.concat(arr1);
+    if (arr2.length) output = output.concat(arr2);
     return output;
   }
 
   function mergeSort(array) {
-    var output = [];
-    var arr1 = split(array)[0];
-    var arr2 = split(array)[1];
-    if(arr1.length >= 1){
-      output = mergeSort(arr1).concat(mergeSort(arr2));
-    } else {
-      output = merge(arr1, arr2);
+    if (array.length > 1) {
+      var arrLeft = split(array)[0];
+      var arrRight = split(array)[1];
+      return merge(mergeSort(arrLeft), mergeSort(arrRight));
     }
-    return output;
-    console.log(merge(arr1, arr2));
-    }
+    return array;
+  }
